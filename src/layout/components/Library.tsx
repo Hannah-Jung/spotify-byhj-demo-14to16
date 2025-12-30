d4b173bcf913d0f8ed7260499069aec9e5ce2441
Hannah Jung <hj.hannahjung@gmail.com>
2025-12-29 22:53:11 -0800

    Updated Library.tsx

--- src/layout/components/Library.tsx
+++ src/layout/components/Library.tsx
@@ -8,19 +8,22 @@ import useGetCurrentUserProfile from '../../hooks/useGetCurrentUserProfile'
 import { useInView } from 'react-intersection-observer'
 import { useEffect } from 'react'
 
-const Library = () => {
-  const { ref, inView } = useInView();
-  const {data, isLoading, error, fetchNextPage, hasNextPage, isFetchingNextPage} = useGetCurrentUserPlaylists({limit:10, offset:0})
+const Library = () => {  
   const {data:user} = useGetCurrentUserProfile()
-  useEffect(()=> {
+  if (!user) return <EmptyPlaylist/>
+  const { ref, inView } = useInView({
+  rootMargin: '100px',threshold: 0});
+  const {data, isLoading, error, fetchNextPage, hasNextPage, isFetchingNextPage} = useGetCurrentUserPlaylists({limit:10, offset:0})
+  
+  useEffect(()=> {    
     if(inView && hasNextPage && !isFetchingNextPage){
       fetchNextPage()
     }
-  },[inView, fetchNextPage, hasNextPage, isFetchingNextPage])
-  if (!user) return <EmptyPlaylist/>
+  // },[inView, fetchNextPage, hasNextPage, isFetchingNextPage])
+  },[inView, hasNextPage, isFetchingNextPage])
+  
   if (isLoading) return <LoadingSpinner/>
   if (error) return <ErrorMessage errorMessage={error.message}/>
-  console.log("current user playlists: ", data)
 
   const playlists = data?.pages.flatMap(page => page.items) || []
   return (
